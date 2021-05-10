124cfacabbb755500543b301bc4e48aa0a339d89
dxmdeveloper <74650257+dxmdeveloper@users.noreply.github.com>
2021-05-10 13:40:50 +0200

    Delete backend directory

--- backend/DB/Entity.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/backend/paths.php");
-
-abstract class Entity
-{
-    abstract public function getId();
-}
--- backend/paths.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("ROOT_DIR", $_SERVER['DOCUMENT_ROOT']);
-define("BACKEND_DIR",$_SERVER['DOCUMENT_ROOT']."/backend");
-define("DB_DIR", ROOT_DIR."/backend/DB");
